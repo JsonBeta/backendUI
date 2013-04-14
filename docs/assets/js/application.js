8df68c3afcf16085c30c1ffb9ffe58552a841eae
alex <alex@10.237.52.7>
2013-04-15 02:02:13 +0800

    完善BACKENDUI的中文API文档--JAVASCRIPT插件文档

--- docs/assets/js/application.js
+++ docs/assets/js/application.js
@@ -143,6 +143,35 @@
       })
     })
 
+    //modal DEMO演示绑定的事件
+$('#backdroptrue').on('click',function(evt){
+    $('#modalbackdroptrue').modal({
+      backdrop:true
+    });
+  });
+
+  $('#backdropfalse').on('click',function(evt){
+    $('#modalbackdropfalse').modal({
+      backdrop:false
+    });
+  });
+
+  $('#keyboardtrue').on('click',function(evt){
+    $('#modalkeyboardtrue').modal({
+      keyboard:true
+    });
+  });
+
+  $('#keyboardfalse').on('click',function(evt){
+    $('#modalkeyboardfalse').modal({
+      keyboard:false
+    });
+  });
+
+  $('#amodaltoggle').on('click',function(evt){
+    $('#modaltoggle').modal('toggle');
+  });
+
   })
 
 // Modified from the original jsonpi https://github.com/benvinegar/jquery-jsonpi
